645a0cdee9efab6c8f3c6fab17966bb01820ab74
tpaing00 <tpaing00@mylangara.ca>
2026-09-01 08:19:31 -0700

    add Mobile initial

--- src/Restaurant.Mobile/MauiProgram.cs
+++ src/Restaurant.Mobile/MauiProgram.cs
@@ -1,28 +1,50 @@
-﻿using Microsoft.AspNetCore.Components.WebView.Maui;
-using Restaurant.Mobile.Data;
+﻿using Microsoft.Extensions.Logging;
+using Restaurant.UI.Shared.Services;
+using Microsoft.EntityFrameworkCore;
 
-namespace Restaurant.Mobile
+namespace Restaurant.Mobile;
+
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                });
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
 
-            builder.Services.AddMauiBlazorWebView();
+        builder.Services.AddMauiBlazorWebView();
+
 #if DEBUG
-    		builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Services.AddBlazorWebViewDeveloperTools();
+        //builder.Logging.AddDebug();
 #endif
 
-            builder.Services.AddSingleton<WeatherForecastService>();
+        // API Base URL - your PC's IP address
+        var apiBaseUrl = "http://10.0.2.2:5000/";
 
-            return builder.Build();
-        }
+        // Add HTTP client for API
+        builder.Services.AddScoped(sp =>
+        {
+            var handler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback =
+                    HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+            };
+            return new HttpClient(handler) { BaseAddress = new Uri(apiBaseUrl) };
+        });
+
+        // Add API service
+        builder.Services.AddScoped<RestaurantApiService>();
+
+        // Add local SQLite database (we'll create this next)
+        var dbPath = Path.Combine(FileSystem.AppDataDirectory, "restaurant.db");
+        // builder.Services.AddDbContext<LocalDbContext>(options =>
+        //     options.UseSqlite($"Data Source={dbPath}"));
+
+        return builder.Build();
     }
 }
